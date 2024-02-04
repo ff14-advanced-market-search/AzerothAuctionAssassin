fb1362fbfc800ce7ca4b3637a1cfff3c204b11ab
cohenaj194 <cohenaj194@gmail.com>
2024-02-03 23:11:27 -0500

    add classic russians

--- utils/helpers.py
+++ utils/helpers.py
@@ -18,7 +18,7 @@ def create_oribos_exchange_item_link(realm_name, item_id, region):
 
 def get_wow_russian_realm_ids():
     retail_realms = [1602, 1604, 1605, 1614, 1615, 1623, 1923, 1925, 1928, 1929, 1922]
-    classic_realms = []
-    sod_realms = [5280,5285,5829,5830]
+    classic_realms = [4452, 4474]
+    sod_realms = [5280, 5285, 5829, 5830]
     return retail_realms + classic_realms + sod_realms
 
